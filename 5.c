c111b6dccfbf829b24c9ad49563856fb4ba7da18
15EC048 <brindhasss.mk6@gmail.com>
2018-02-20 11:14:04 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,n3;
+printf("Enter the numbers:");
+scanf("%d",&n1);
+scanf("%d",&n2);
+scanf("%d",&n3);
+if(n1>n2)
+{
+printf("n1 is larger");
+}
+else
+{
+printf(
